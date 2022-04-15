bb4b888c9d2fb89b32a338922cedf01998945b9f
Arief <riff.sense@gmail.com>
2022-04-16 00:54:20 +0700

    livewire data binding

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Livewire Screencasts</title>
+        @livewireStyles
+    </head>
+    <body>
+        @livewire('hello-world')
+
+        @livewireScripts
+    </body>
+</html>
